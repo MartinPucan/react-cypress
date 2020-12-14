07f7c39f18fa5e14176b73b57ceb8fa0a7397b1d
MartinPucan <pucan.m@seznam.cz>
2020-12-14 01:50:30 +0100

    just fun - remove project - christmas countdown

--- cypress/integration/counter/counter.spec.js
+++ cypress/integration/counter/counter.spec.js
@@ -8,10 +8,10 @@ context('Counter', () => {
 	})
 
 	it('Test counter', () => {
-		cy.get('.container-fluid').should('be.visible')
-		cy.get("[data-cy='btn-increment']").click()
-		cy.get("[data-cy='counter']").contains('1')
-		cy.get("[data-cy='btn-reset']").click()
-		cy.get("[data-cy='counter']").contains('0')
+		// cy.get('.container-fluid').should('be.visible')
+		// cy.get("[data-cy='btn-increment']").click()
+		// cy.get("[data-cy='counter']").contains('1')
+		// cy.get("[data-cy='btn-reset']").click()
+		// cy.get("[data-cy='counter']").contains('0')
 	})
 })
--- src/App.js
+++ src/App.js
@@ -1,16 +1,82 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './App.css';
-import Counter from "./components/Counter";
-import NavBar from "./components/NavBar.js";
+import Tree from './christmas-tree.svg';
+// import Counter from "./components/Counter";
+// import NavBar from "./components/NavBar.js";
 
 function App() {
+	const calculateTimeLeft = () => {
+		let year = new Date().getFullYear();
+		const difference = +new Date(`${year}-12-24`) - +new Date();
+		let timeLeft = {};
+
+		if (difference > 0) {
+			timeLeft = {
+				days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+				hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+				minutes: Math.floor((difference / 1000 / 60) % 60),
+				seconds: Math.floor((difference / 1000) % 60),
+			};
+		}
+
+		return timeLeft;
+	};
+
+	const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+	const [year] = useState(new Date().getFullYear());
+	const timerComponents = [];
+
+	useEffect(() => {
+		setTimeout(() => {
+			setTimeLeft(calculateTimeLeft());
+		}, 1000);
+	}, []);
+
+
+	Object.keys(timeLeft).forEach((interval) => {
+		if (!timeLeft[interval]) {
+			return;
+		}
+
+		timerComponents.push(
+			<span>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+		);
+	});
+
   return (
-    <div className="App">
-			<NavBar />
-			<div className="container-fluid">
-      	<Counter />
+    <div className="App bg-danger">
+			{/*<NavBar />*/}
+			{/*<div className="container-fluid">*/}
+
+			<div className="card text-white bg-danger mb-3">
+				<div className="card-header d-flex justify-content-around align-items-center">
+					<img src={Tree} width={64} alt="christmas-tree" />
+					<h3 className="text-center">Christmas Countdown {year}</h3>
+					<img src={Tree} width={64} alt="christmas-tree" />
+				</div>
+				<div className="card-body">
+					<div className="d-flex col justify-content-center p-5">
+						<div className="m-2">
+							<h1> {timeLeft.days}d</h1>
+						</div>
+						<div className="m-2">
+							<h1> {timeLeft.hours}h</h1>
+						</div>
+						<div className="m-2">
+							<h1> {timeLeft.minutes}m</h1>
+						</div>
+						<div className="m-2">
+							<h1> {timeLeft.seconds}s</h1>
+						</div>
+					</div>
+				</div>
+
+				{/*<Counter />*/}
+				{/*<hr />*/}
 			</div>
-    </div>
+		</div>
   );
 }
 
